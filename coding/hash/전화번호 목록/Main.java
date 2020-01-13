18591c783fee3d4e6aeb6279bf50f9acbd555e28
SoonYoung Jung <hellosnon@naver.com>
2020-01-13 15:02:30 +0900

    Main.java

--- coding/hash/전화번호 목록/Main.java
+++ coding/hash/전화번호 목록/Main.java
@@ -1,1 +1,32 @@
+import java.util.HashMap;
+import java.util.Arrays;
 
+public class Solution {
+
+    public boolean solution(String[] phone_book) {
+        
+        boolean answer = true;
+        
+        // 문자열 오름차순 정렬
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            
+            // 다음 배열값의 접두에 내가 포함되었는가?
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static void main(String []args){
+        
+        Solution sol = new Solution();
+        String[] book = new String[] {"119", "97674223", "1195524421"};
+        
+        System.out.println(sol.solution(book));
+    }
+    
+}
---
+++ coding/hash/전화번호 목록/README.md
@@ -0,0 +1,7 @@
+문제 링크
+
+- https://programmers.co.kr/learn/courses/30/lessons/42577
+
+해결
+
+- 문자열 정렬 후 startWith 함수로 i번째 요소와 i+1번째 요소 비교
